bea61393c640b882d8ba94cf8480e1157dd45957
Isha <ishac1017@gmail.com>
2025-06-05 09:57:12 -0500

    created a transition from content view

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -9,15 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Text("This is the root view  🌳")
+                
+                NavigationLink(destination: Text("You've arrived to the Second View 🎊")) {
+                    Text("Click me!")
+                }
+                //end nav link
+            }
+            //end vstack
         }
-        .padding()
+        //end navStack
     }
+    //end body
 }
+//end content view
 
 #Preview {
     ContentView()
